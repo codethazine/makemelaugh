6af6280684b5d2d195ecd68ddf8891f3cb0563a7
codethazine <dario.finocchiaro01@gmail.com>
2024-01-26 21:20:24 +0100

    Setup decent homepage

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -11,10 +11,6 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
--- webapp/src/views/HomeView.vue
+++ webapp/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img class="logo-home" alt="logo" src="../assets/logo.png">
     <router-link to="/game">
       <button class="btn-8bit">START</button>
     </router-link>
@@ -15,36 +15,42 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-image: url('../assets/home-bg.png'); /* Adding background image */
+  background-size: cover, contain; /* Cover the entire background */
+  background-position: center; /* Centers the background image */
 }
 
+.logo-home{
+  width: 40vh;
+  height: auto;
+  margin-bottom: 20px;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+
 .btn-8bit {
-  font-family: 'Press Start 2P'; /* This is a commonly used 8-bit style font */
-  font-size: 16px;
+  font-family: 'VT323', monospace; /* VT323 font applied */
+  font-size: 30px; /* Adjust font size as needed */
   color: #fff;
-  background-color: #333;
-  border: 3px solid #555;
+  background-color: #32CD32; /* Green background */
+  border: 3px solid #228B22; /* Darker green border */
   border-radius: 5px;
-  padding: 10px 20px;
+  padding: 10px 50px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
   margin: 4px 2px;
   cursor: pointer;
-  -webkit-transition-duration: 0.4s; /* Safari */
-  transition-duration: 0.4s;
+  -webkit-transition-duration: 0.2s; /* Safari */
+  transition-duration: 0.2s;
   text-shadow: 0 0 3px #000;
-  box-shadow: 0 9px #999;
+  box-shadow: 0 9px #006400; /* Deep green shadow for depth */
 }
 
 .btn-8bit:hover {
-  background-color: #555;
+  background-color: #2E8B57; /* Slightly different green on hover */
   color: white;
-  box-shadow: 0 5px #666;
+  box-shadow: 0 5px #006400;
   transform: translateY(4px);
-}
-
-@font-face {
-  font-family: 'Press Start 2P';
-  src: url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
 }
 </style>
