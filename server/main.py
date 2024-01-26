23a224137d28120cbbd8ea2a7889a2f75d03a9dc
josura <giorgiolocicero@hotmail.com>
2024-01-26 22:14:57 +0100

    handle more connections (5 queued maximum), added face and smile detection, although not quantifiable

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-# TODO: Add server elaborating happiness frames and returning score 0-1 to client
+# TODO: Add server elaborating happiness frames and returning score 0-1 to clients
 # client send a frame to server
 # server return an happiness score to client
 # client send a frame every second
@@ -9,12 +9,59 @@ import sys
 import cv2
 import pickle
 import numpy as np
+import threading
 import time  # Added for timeout functionality
 
 def calculate_happiness_score(frame):
     # TODO: Calculate happiness score for frame
-    # use cv2 to detect face
+    # use cv2 to detect face and smile, using haar cascade
+
+    # 0.0: sad
+    # 0.5: neutral
+    # 1.0: happy
+
+    # pretrained models need to be already on the server
+    face_cascadeClassifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    smile_cascadeClassifier = cv2.CascadeClassifier('haarcascade_smile.xml')
+
     return np.random.rand()  
+
+def handle_client(connection):
+    try:
+        # Set a timeout for the connection (1 second in this case)
+        connection.settimeout(1.0)
+
+        while True:
+            # Receive frame from client
+            frame_data = b''
+            while True:
+                packet = connection.recv(4096)
+                if not packet:
+                    break
+                frame_data += packet
+
+            if not frame_data:
+                break  # No more data, exit loop
+
+            # Deserialize frame data
+            frame = pickle.loads(frame_data)
+
+            # Calculate happiness score for the frame
+            happiness_score = calculate_happiness_score(frame)
+
+            # Send happiness score back to client
+            connection.sendall(str(happiness_score).encode())
+
+    except socket.timeout:
+        print("Connection timed out")
+        connection.close()
+
+    except Exception as e:
+        print("Error:", e)
+
+    finally:
+        # Clean up the connection
+        connection.close()
 
 
 def main():
@@ -27,50 +74,18 @@ def main():
     sock.bind(server_address)
 
     # Listen for incoming connections
-    sock.listen(1)
+    sock.listen(5) # 5 is the maximum number of queued connections
 
     while True:
         # Wait for a connection
         print('waiting for a connection')
         connection, client_address = sock.accept()
 
-        try:
-            print('connection from', client_address)
+        print('connection from', client_address)
 
-            # Set a timeout for the connection (1 second in this case)
-            connection.settimeout(1.0)
-
-            while True:
-                # Receive frame from client
-                frame_data = b''
-                while True:
-                    packet = connection.recv(4096)
-                    if not packet:
-                        break
-                    frame_data += packet
-
-                if not frame_data:
-                    break  # No more data, exit loop
-
-                # Deserialize frame data
-                frame = pickle.loads(frame_data)
-
-                # Calculate happiness score for the frame
-                happiness_score = calculate_happiness_score(frame)
-
-                # Send happiness score back to client
-                connection.sendall(str(happiness_score).encode())
-
-        except socket.timeout:
-            print("Connection timed out")
-            connection.close()
-
-        except Exception as e:
-            print("Error:", e)
-
-        finally:
-            # Clean up the connection
-            connection.close()
+        # Start a new thread to handle the connection
+        client_handler = threading.Thread(target=handle_client, args=(connection,))
+        client_handler.start()
 
 if __name__ == "__main__":
     main()
