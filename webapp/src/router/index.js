77efdc69b761830499f5e3a54af9133048a8619e
codethazine <dario.finocchiaro01@gmail.com>
2024-01-26 19:41:12 +0100

    Init GameView

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -8,13 +8,13 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/game',
+    name: 'game',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (game.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      return import(/* webpackChunkName: "game" */ '../views/GameView.vue')
     }
   }
 ]
