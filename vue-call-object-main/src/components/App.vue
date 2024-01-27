ad139eb74c5320d01ee2d2dad1e4771dd0b60201
codethazine <dario.finocchiaro01@gmail.com>
2024-01-27 19:52:34 +0100

    Init shitty vue app

---
+++ vue-call-object-main/src/components/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <app-header />
+  <home-screen v-if="appState === 'idle'" :join-call="joinCall" />
+  <call-tile
+    v-else-if="appState === 'incall'"
+    :leave-call="leaveCall"
+    :name="name"
+    :room-url="roomUrl"
+  />
+</template>
+
+<script>
+import CallTile from "./CallTile.vue";
+import AppHeader from "./AppHeader.vue";
+import HomeScreen from "./HomeScreen.vue";
+
+export default {
+  name: "App",
+  components: {
+    CallTile,
+    AppHeader,
+    HomeScreen,
+  },
+  data() {
+    return {
+      appState: "idle",
+      name: "Guest",
+      roomUrl: null,
+    };
+  },
+  methods: {
+    /**
+     * Set name and URL values entered in Home.vue form in data obj
+     */
+    joinCall(name, url) {
+      this.name = name;
+      this.roomUrl = url;
+      this.appState = "incall";
+    },
+    // Reset app state to return to the home screen after leaving call
+    leaveCall() {
+      this.appState = "idle";
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow-x: hidden;
+  background-color: #121a24;
+}
+a {
+  text-decoration: none;
+  color: #2c3e50;
+  display: flex;
+  align-items: center;
+}
+body {
+  margin: 0;
+}
+</style>
---
+++ vue-call-object-main/src/components/AppHeader.vue
@@ -0,0 +1,91 @@
+<template>
+  <header>
+    <div>
+      <div class="row">
+        <a class="right-border" href="https://daily.co" target="_blank">
+          <img class="icon" :src="daily" alt="Daily.co" />
+        </a>
+        <img class="icon" src="../assets/logo.png" alt="Daily.co" />
+        <p>Call object Vue Demo</p>
+      </div>
+      <div class="row">
+        <div class="right-border">
+          <a
+            target="_blank"
+            class="button-link"
+            href="https://docs.daily.co/reference/daily-js"
+            rel="noreferrer noopener"
+          >
+            <span>API docs</span>
+            <img :src="newtab" alt="Open in new tab" />
+          </a>
+        </div>
+        <a
+          href="https://github.com/daily-demos/vue-call-object"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <img class="icon" :src="github" alt="Github repo" />
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import github from "../assets/github.svg";
+import daily from "../assets/daily.svg";
+import newtab from "../assets/newtab.svg";
+
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      github,
+      daily,
+      newtab,
+    };
+  },
+};
+</script>
+
+<style>
+header {
+  font-size: 12px;
+  line-height: 16px;
+  font-weight: bold;
+  border-bottom: 1px solid #c8d1dc;
+  padding: 2px;
+  background-color: #fff;
+}
+header > div {
+  max-width: 1200px;
+  margin: auto;
+  padding: 0 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.icon {
+  height: 24px;
+}
+.row {
+  display: flex;
+  align-items: center;
+}
+.right-border {
+  padding-right: 16px;
+  border-right: 1px solid #c8d1dc;
+  margin-right: 16px;
+}
+.button-link {
+  border: 1px solid #c8d1dc;
+  border-radius: 8px;
+  padding: 7px 16px;
+}
+@media screen and (max-width: 700px) {
+  .button-link {
+    display: none;
+  }
+}
+</style>
